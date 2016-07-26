20695fea35d1c771d3174dc001def90d5e20bc86
Wim Looman <wim@nemo157.com>
2016-07-26 19:04:06 +0200

    make client work from file://

--- public/app.js
+++ public/app.js
@@ -1,15 +1,15 @@
 requirejs.config({
     paths: {
-        jquery: '//code.jquery.com/jquery-1.10.2.min',
-        bootstrap: '//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min',
-        lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
-        fabric: '//cdnjs.cloudflare.com/ajax/libs/fabric.js/1.4.0/fabric.min',
-        postal: '//cdnjs.cloudflare.com/ajax/libs/postal.js/0.8.5/postal.min',
-        knockout: '//cdnjs.cloudflare.com/ajax/libs/knockout/3.0.0/knockout-min',
-        'knockout-projections': '//rawgithub.com/mariusGundersen/knockout-projections/amd/dist/knockout-projections-1.0.0.min',
-        'knockout.mapping': '//cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.4.1/knockout.mapping',
-        jStorage: '//cdnjs.cloudflare.com/ajax/libs/jStorage/0.4.4/jstorage.min',
-        'knockout-bootstrap': '//rawgithub.com/billpull/knockout-bootstrap/master/build/knockout-bootstrap.min'
+        jquery: 'https://code.jquery.com/jquery-1.10.2.min',
+        bootstrap: 'https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min',
+        lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
+        fabric: 'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.4.0/fabric.min',
+        postal: 'https://cdnjs.cloudflare.com/ajax/libs/postal.js/0.8.5/postal.min',
+        knockout: 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.0.0/knockout-min',
+        'knockout-projections': 'https://rawgithub.com/mariusGundersen/knockout-projections/amd/dist/knockout-projections-1.0.0.min',
+        'knockout.mapping': 'https://cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.4.1/knockout.mapping',
+        jStorage: 'https://cdnjs.cloudflare.com/ajax/libs/jStorage/0.4.4/jstorage.min',
+        'knockout-bootstrap': 'https://rawgithub.com/billpull/knockout-bootstrap/master/build/knockout-bootstrap.min'
     },
     map: {
         '*': {
